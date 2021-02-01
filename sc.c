981dd1ad0ceb31da12a7f021fff3afc694659a52
altbrace <dim203286@gmail.com>
2021-02-01 11:25:30 +0700

    Some memory managing functions implemented

--- sc.c
+++ sc.c
@@ -3,6 +3,42 @@
 
 int sc_memoryInit() {
 	
+	extern int memory[100];
 
+	for (int i = 0; i < 100; i++) memory[i] = 0;
+
+	return 0;
+}
+
+int sc_memorySet(int addr, int val) {
+	
+	if (addr < 0 || addr > 99) return -1;
+	
+	memory[addr] = val;
+
+	return 0;
+
+}
+
+int sc_memoryGet(int addr, int* val) {	
+	
+	if (addr < 0 || addr > 99) return -1;
+
+	&val = memory[addr];
+
+	return 0;
+}
+
+int sc_memorySave(char* filename) {
+	
+	FILE* file = fopen(filename, "wb");
+	if (file == NULL) return -1;
+
+	size_t count = fwrite(memory, sizeof(int), 1, file);
+	fclose(file);
+
+	if (count != 100) return -1;
+
+	return 0;
 }
 
