e87b2a4de893577e9675dc0139e4228413599e0e
altbrace <dim203286@gmail.com>
2021-02-15 08:22:30 +0700

    bruh commit

--- sc.c
+++ sc.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "sc.h"
+
+#define FIRST_BIT 0b00010000
+#define SECOND_BIT 0b00001000
+#define THIRD_BIT 0b00000100
+#define FOURTH_BIT 0b00000010
+#define FIFTH_BIT 0b00000001
+
+extern int memory[100];
+extern uint8_t flags;
 
 int sc_memoryInit() {
 	
-	extern int memory[100];
-
 	for (int i = 0; i < 100; i++) memory[i] = 0;
 
 	return 0;
@@ -24,7 +32,7 @@ int sc_memoryGet(int addr, int* val) {
 	
 	if (addr < 0 || addr > 99) return -1;
 
-	val = memory[addr];
+	*val = memory[addr];
 
 	return 0;
 }
@@ -57,6 +65,16 @@ int sc_memoryLoad(char* filename) {
 
 int sc_regInit(void) {
 	
+	flags = 0b00000000;
+	return 0;
+}
 
+int sc_regSet(uint8_t reg, int value) {
+	
+	if (value == 1) flags |= reg;
+	else if (value == 0) flags = flags & (~reg);
+	else return -1;
+
+	return 0;
 }
 
--- sc.h
+++ sc.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdint.h>
 
 #ifndef SC_H
 #define SC_H
@@ -10,8 +11,8 @@ int sc_memoryGet(int addr, int* val);
 int sc_memorySave(char* filename);
 int sc_memoryLoad(char* filename);
 int sc_regInit(void);
-int sc_regSet(int reg, int val);
-int sc_regGet(int reg, int* val);
+int sc_regSet(uint8_t reg, int val);
+int sc_regGet(uint8_t reg, int* val);
 int sc_commandEncode(int cmd, int operand, int* value);
 int sc_commandDecode(int value, int* cmd, int* operand);
 
