7b3810b4846b20bdfbedead4d31197cf62c1c39a
altbrace <dim203286@gmail.com>
2021-03-01 08:29:23 +0700

    .

---
+++ src/myBigChars/mbc.c
@@ -0,0 +1,10 @@
+#include "mbc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int bc_printA(char* str) {
+
+	printf("^N");
+	return 0;
+
+}
--- src/myBigChars/mbc.h
+++ src/myBigChars/mbc.h
@@ -3,7 +3,16 @@
 #ifndef MBC_H
 #define MBC_H
 
+enum colors {black = 30, red = 31, green = 32,
+            yellow = 33, blue = 34, purple = 35,
+            cyan = 36, white = 37};
+
 int bc_printA(char* str);
 int bc_box(int x1, int y1, int x2, int y2);
-int bc_printbigchar(char c, int x, int y, enum colors color, enum colors color);
-int bc_setbigcharpos(int* big)
+int bc_printbigchar(char c, int x, int y, enum colors fgcolor, enum colors bgcolor);
+int bc_setbigcharpos(int* big, int x, int y, int value);
+int bc_getbigcharpos(int* big, int x, int y, int* count);
+int bc_bigcharwrite(FILE* file, int* big, int count);
+int bc_bigcharread(FILE* file, int* big, int needed, int* read);
+
+#endif
